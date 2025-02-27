0d725a6e82d8e40c9564f74e829c3f5b097151b9
Jhon Edwin Monsalve <jedwinmonsalve@gmail.com>
2025-02-26 20:48:06 -0500

    Modificado el titulo del archivo

--- calculadoraIMC.php
+++ calculadoraIMC.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calculadora IMC</title>
+    <title>Calculadora IMC con PHP</title>
 </head>
 <body>
     <h1>Calculadora IMC</h1>
